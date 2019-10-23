7664231961690d78626ca97c5d2da680a6673fa0
Rexben001 <rexben.rb@gmail.com>
2019-10-23 13:55:14 +0100

    Add serializer to convert quersets to python datatypes

---
+++ adventure/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+# Serializers allow complex data such as querysets and model instances to be converted to native Python datatypes
+class Serializer(serializers.Serializer):
+    title = serializers.CharField(max_length=50, default="DEFAULT TITLE")
+    description = serializers.CharField(max_length=500, default="DEFAULT DESCRIPTION")
+    n_to = serializers.IntegerField(default=0)
+    s_to = serializers.IntegerField(default=0)
+    e_to = serializers.IntegerField(default=0)
+    w_to = serializers.IntegerField(default=0)
